55590646b74a21d6f3d23aac337f0ef06cb2465c
SMJ426 <jerry04260@gmail.com>
2023-08-24 16:44:43 +0900

    rocare

---
+++ src/main/java/com/example/movies/dto/UserDto.java
@@ -0,0 +1,20 @@
+package com.example.movies.dto;
+
+import com.example.movies.domain.User.Role;
+import lombok.Builder;
+import lombok.Data;
+
+
+@Data
+@Builder
+
+public class UserDto {
+    private String email;
+    private String userName;
+    private String password;
+    private String birth;
+    private String phoneNum;
+    private String address;
+    private Role role;
+
+}
